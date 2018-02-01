8d7dfa5d2fb24d034c91aff45b93a8b86444c472
ishanverma1 <ishaniv123@gmail.com>
2018-02-01 17:08:09 -0500

    hw i

---
+++ cmake-build-debug/CMakeFiles/cs205.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cs205.dir/main.cpp.o"
+  "cs205.pdb"
+  "cs205"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cs205.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
