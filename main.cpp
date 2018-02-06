ee2040e9837ffaba26b967b33fd72fe22bfcd554
ishanverma1 <ishaniv123@gmail.com>
2018-02-06 11:07:04 -0500

    first commit by Ishan

--- main.cpp
+++ main.cpp
@@ -3,5 +3,6 @@
 int main() {
     std::cout << "Hello, World Ishan!" << std::endl;
     std::cout << "Peter was here." << std::endl;
+    std::cout << "Ishan was here - Feb 6, 2018." << std::endl;
     return 0;
 }
